a66ae02fe30cff2cea7869d8c1a13f7c2826a466
Jacob Hill <jacob.hill@chde.qld.gov.au>
2026-02-28 07:27:25 +1000

    add alb module

---
+++ modules/alb/main.tf
@@ -0,0 +1,51 @@
+resource "aws_lb" "public_alb" {
+  name               = "public-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_groups
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "alb-target-group"
+  port     = 8025
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+  }
+
+  tags = {
+    Name = "main"
+  }
+
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.public_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+
+}
+
+resource "aws_lb_target_group_attachment" "aws_lb_target_group" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = var.target_ec2_instance_id
+  port             = var.target_ec2_port
+}
